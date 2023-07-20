96661b581ce05855658c61c43ef4cfd33eb3390b
Brandon <loeffler.1287@gmail.com>
2023-07-19 23:06:41 -0500

    Updates to mod

--- Initalization.lua
+++ Initalization.lua
@@ -1,3 +1,41 @@
 function (modTable)
-    --insert code here
+    modTable.spells = {
+        {
+            spellName = "Moonfire_player",
+            color = modTable.config.moonfireColor,
+            trackSpell = true,
+        },
+        {
+            spellName = "Sunfire_player",
+            color = modTable.config.sunfireColor,
+            trackSpell = true,
+        },
+        {
+            spellName = "Stellar Flare_player",
+            color = modTable.config.stellarColor,
+            trackSpell = modTable.config.trackStellar,
+        },
+        {
+            spellName = "Fungal Growth_player",
+            color = modTable.config.shroomColor,
+            trackSpell = modTable.config.trackShroom,
+        }
+    }
+    
+    
+    modTable.SetColor =  function (unitFrame, color)        
+        
+        if (modTable.config.sborder) then
+            Plater.SetBorderColor(unitFrame, color)           
+        end
+        
+        if (modTable.config.splate) then        
+            Plater.SetNameplateColor(unitFrame, color)
+        end
+        
+        if (modTable.config.sname) then
+            local npcName = unitFrame.healthBar.unitName            
+            Plater:SetFontColor (npcName, color)
+        end
+    end
 end
--- Nameplate_Updated.lua
+++ Nameplate_Updated.lua
@@ -1,22 +1,13 @@
 function (self, unitId, unitFrame, envTable, modTable)  
-    local spellIDs = envTable.spellIDs
-    local trackStellar = modTable.config.trackStellar
-    local trackShroom = modTable.config.trackShroom
-    local trackPVP = modTable.config.trackPVP
-    
-    local hasmf
-    local hassf
-    local hasfl
-    local hasShroom
-    
+    local spells = modTable.spells
     
     -- Do nothing if the player isn't in combat
     if(not Plater.IsInCombat() and not trackPVP) then
-        return
+        return;
     end
     -- Do nothing if the unit isn't in combat
     if(not unitFrame.InCombat and not trackPVP) then
-        return
+        return;
     end
     
     -- Only work in PvE
@@ -25,51 +16,13 @@ function (self, unitId, unitFrame, envTable, modTable)
     end
     
     
-    
-    if (Plater.UnitHasAura(unitFrame, "Moonfire_player")) then
-        hasmf = true
-    else
-        hasmf = false
-    end
-    
-    if (Plater.UnitHasAura(unitFrame, "Sunfire_player")) then
-        hassf = true
-    else
-        hassf = false
-    end
-    
-    if (Plater.UnitHasAura(unitFrame, "Stellar Flare_player")) then
-        hasfl = true
-    else
-        hasfl = false
-    end
-    
-    if (Plater.UnitHasAura(unitFrame, "Fungal Growth_player")) then
-        hasShroom = true
-    else
-        hasShroom = false
-    end
-    
-    -- Sets NamePlate color to missing Moonfire color and stops
-    if (not hasmf) then
-        Plater.SetNameplateColor (unitFrame, modTable.config.moonfireColor)
-        return;
-    end
-    
-    -- Sets NamePlate color to missing Sunfire color and stops
-    if (not hassf) then
-        Plater.SetNameplateColor (unitFrame, modTable.config.sunfireColor)
-        return;
-    end
-    
-    if (not hasfl and trackStellar) then
-        Plater.SetNameplateColor (unitFrame, modTable.config.stellarColor)
-        return;
-    end
-    
-    if (not hasShroom and trackShroom) then
-        Plater.SetNameplateColor (unitFrame, modTable.config.shroomColor)
-        return;
+    -- Check each spell in the spell table and see if it is applied. If it is, set the color.
+    for k,v in pairs(spells) do
+        if(not Plater.UnitHasAura(unitFrame, v.spellName) and v.trackSpell) then
+            modTable.SetColor(unitFrame, v.color)
+            break;
+        else
+            modTable.SetColor(unitFrame)
+        end
     end
 end
-
